a216485af60be43d47d932b1ff07ec30c5c90093
Michael Rubanov <michael.r@anzu.io>
2018-06-21 15:50:01 +0300

    typo fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const collectDefaultMetrics = client.collectDefaultMetrics;
 const register = client.register;
 
 module.exports = function (timeout = 5000) {
-  collectDefaultMetrics({ timeout: 5000 });
+  collectDefaultMetrics({ timeout});
   return function (req, res, next) {
     res.statusCode = 200;
     res.setHeader('Content-Type', register.contentType);
